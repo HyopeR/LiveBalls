1321e731775e3938a8a9941e24ef6a8a641a69c8
HyopeR <tol_celik@hotmail.com>
2020-04-17 14:52:43 +0300

    indexFactory socket connection method

---
+++ public/javascripts/controller/indexController.js
@@ -0,0 +1,15 @@
+app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
+
+    const connectionOptions = {
+        reconnectionAttempts: 3,
+        reconnectionDelay: 600
+    };
+
+    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+        .then((socket) => {
+            console.log('Connected to the service.', socket);
+        }).catch((err) => {
+        console.log(err)
+        });
+
+}]);
